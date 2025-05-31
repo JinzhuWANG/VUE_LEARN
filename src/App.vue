f19dafaa6e03227fb2d8757d9404fa13a67c317d
jinzhu <wangjinzhulala@gmail.com>
2025-05-31 15:12:37 +1000

    firt commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+</script>
+
+
+
+<template>
+  <Navbar />
+  <Hero title="test" sub_title="test sub" />
+  <HomeCards />
+</template>
